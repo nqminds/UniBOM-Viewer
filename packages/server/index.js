62dc59deb08533b5d0c8f3e5ec7b2e3c3256eb7f
Ash Setter <ash@nquiringminds.com>
2023-03-22 16:07:10 +0000

    feat: added command line arguments to specify parameters for script

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,26 +1,55 @@
 const express = require('express')
 const app = express()
-const port = 3000
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const serverPort = 3000
+const {promisify, parseArgs} = require('util');
+const exec = promisify(require('child_process').exec);
 
-const userID = "me";
-const key = "my-ssh-key";
-const IP = "192.168.0.1";
-const scriptPort = "4001";
+const cmdLineOptions = {
+  userID: {
+    type: 'string',
+    default: "me",
+  },
+  key: {
+    type: 'string',
+    default: "my-ssh-key",
+  },
+  ip:{
+    type: 'string',
+    default: "192.168.0.1",
+  },
+  port: {
+    type: 'string',
+    default: "4001",
+  },
+};
+
+const {values} = parseArgs({options: cmdLineOptions});
+
+const userID = values.userID ?? cmdLineOptions.userID.default;
+const key = values.key ?? cmdLineOptions.key.default;
+const IP = values.ip ?? cmdLineOptions.ip.default;
+const port = values.port ?? cmdLineOptions.port.default;
+
+console.log(userID, key, IP, port);
 
 // we'll either want 4 api routes, one for each permutation
 // or 2 parameters (purecap/hybrid and good/malicious cert) and run the appropriate script for each
 app.get('/run-script', async (req, res) => {
-    let [stdout, stderr, error] = ["", "", ""];
-    try {
-        ({ stdout, stderr } = await exec(`./test.sh ${IP} ${scriptPort} ${userID} ${key}`));
-    } catch (err) {
-        error = err;
-    }
-    res.send({stdout, stderr, error});
+  let [stdout, stderr, error] = ["", "", ""];
+  try {
+    ({ stdout, stderr } = await exec(`./test.sh ${IP} ${port} ${userID} ${key}`));
+  } catch (err) {
+    error = err;
+  }
+  res.send({stdout, stderr, error});
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(serverPort, () => {
+  console.log(`Crypto Demonstrator server listening on port ${serverPort}.
+Using parameters: 
+  userID: ${userID}
+  key: ${key}
+  ip: ${IP}
+  port: ${port}
+  `)
 })
