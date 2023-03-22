a7ecc3b43cef690d877c22cbaa9344a0e07b117d
Ash Setter <ash@nquiringminds.com>
2023-03-22 16:40:34 +0000

    use mjs import syntax and fix console not defined eslint warning

--- packages/server/index.mjs
+++ packages/server/index.mjs
@@ -1,29 +1,31 @@
-const express = require('express')
-const app = express()
-const serverPort = 3000
-const {promisify, parseArgs} = require('util');
-const exec = promisify(require('child_process').exec);
+import express from "express";
+import { promisify, parseArgs } from "util";
+import { exec as execRaw } from "child_process";
+const exec = promisify(execRaw).exec;
+
+const app = express();
+const serverPort = 3000;
 
 const cmdLineOptions = {
   userID: {
-    type: 'string',
+    type: "string",
     default: "me",
   },
   key: {
-    type: 'string',
+    type: "string",
     default: "my-ssh-key",
   },
-  ip:{
-    type: 'string',
+  ip: {
+    type: "string",
     default: "192.168.0.1",
   },
   port: {
-    type: 'string',
+    type: "string",
     default: "4001",
   },
 };
 
-const {values} = parseArgs({options: cmdLineOptions});
+const { values } = parseArgs({ options: cmdLineOptions });
 
 const userID = values.userID ?? cmdLineOptions.userID.default;
 const key = values.key ?? cmdLineOptions.key.default;
@@ -34,14 +36,16 @@ console.log(userID, key, IP, port);
 
 // we'll either want 4 api routes, one for each permutation
 // or 2 parameters (purecap/hybrid and good/malicious cert) and run the appropriate script for each
-app.get('/run-script', async (req, res) => {
+app.get("/run-script", async (req, res) => {
   let [stdout, stderr, error] = ["", "", ""];
   try {
-    ({ stdout, stderr } = await exec(`./test.sh ${IP} ${port} ${userID} ${key}`));
+    ({ stdout, stderr } = await exec(
+      `./test.sh ${IP} ${port} ${userID} ${key}`
+    ));
   } catch (err) {
     error = err;
   }
-  res.send({stdout, stderr, error});
+  res.send({ stdout, stderr, error });
 });
 
 app.listen(serverPort, () => {
@@ -51,5 +55,5 @@ Using parameters:
   key: ${key}
   ip: ${IP}
   port: ${port}
-  `)
-})
+  `);
+});
