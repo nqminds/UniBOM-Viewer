73b2da8bfbae58157216bd065de129e9ce33269c
antmcc49 <antmcc49@gmail.com>
2023-03-28 14:25:44 +0100

    feat(app): implement config file for server addr

---
+++ packages/app/server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const next = require("next");
+const config = require("config");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+
+    server.get(
+      "/api/morello/:purecap(true|false)/:goodCert(true|false)",
+      async (req, res) => {
+        const { purecap, goodCert } = req.params;
+
+        const result = await fetch(
+          `${config.get("serverAddress")}/run-script/${purecap}/${goodCert}`,
+          {
+            referrerPolicy: "strict-origin-when-cross-origin",
+            body: null,
+            method: "GET",
+          }
+        );
+        const data = await result.json();
+        res.send(data);
+      }
+    );
+
+    server.get("*", (req, res) => {
+      return handle(req, res);
+    });
+
+    server.listen(8082, (err) => {
+      if (err) throw err;
+      console.log("Ready on http://localhost:8082");
+    });
+  })
+  .catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
