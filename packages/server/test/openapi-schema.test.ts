1dc05da9df1c0dfd1bcf1c16e67cd841e6b25356
antmcc49 <antmcc49@gmail.com>
2023-04-11 10:57:51 +0100

    chore(server): prettier

--- packages/server/test/openapi-schema.test.ts
+++ packages/server/test/openapi-schema.test.ts
@@ -1,27 +1,25 @@
 import { expect, describe, test, jest } from "@jest/globals";
 
 import { Fetcher } from "openapi-typescript-fetch";
-import {paths} from "../openapi-schema";
-import fetch, { Headers, Request, Response } from 'node-fetch'
-
+import { paths } from "../openapi-schema";
+import fetch, { Headers, Request, Response } from "node-fetch";
 
 if (!globalThis.fetch) {
-  globalThis.fetch = fetch as any
-  globalThis.Headers = Headers as any
-  globalThis.Request = Request as any
-  globalThis.Response = Response as any
+  globalThis.fetch = fetch as any;
+  globalThis.Headers = Headers as any;
+  globalThis.Request = Request as any;
+  globalThis.Response = Response as any;
 }
 
 // declare fetcher for paths
-const fetcher = Fetcher.for<paths>()
+const fetcher = Fetcher.for<paths>();
 
 // global configuration
 fetcher.configure({
-  baseUrl: 'http://localhost:3000',
-})
+  baseUrl: "http://localhost:3000",
+});
 
 describe("", () => {
-
   test("/run-script API endpoint (ts)", async () => {
     // TODO Fix this
     expect(true).toBe(true);
@@ -32,5 +30,5 @@ describe("", () => {
     //   purecap: true, goodCert: false,
     // });
     // expect(status).toEqual(200);
-  })
+  });
 });
