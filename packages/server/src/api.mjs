340aabc30f76bc192dee0f0ca772c9b641196e8b
Ash Setter <ash@nquiringminds.com>
2023-04-13 15:16:06 +0100

    feat: cache scriptPath once setup so we don't need to repeat setup and instatiation.

--- packages/server/src/api.mjs
+++ packages/server/src/api.mjs
@@ -3,6 +3,17 @@ import config from "../config.json" assert { type: "json" };
 
 import scriptPaths from "./script-paths.mjs";
 
+const scriptCache = {
+  purecap: {
+    goodCert: null,
+    maliciousCert: null,
+  },
+  hybrid: {
+    goodCert: null,
+    maliciousCert: null,
+  },
+};
+
 const api = express();
 
 const { username, host, port } = config;
@@ -13,12 +24,10 @@ api.get(
     const { purecap, goodCert } = req.params;
     const mode = purecap === "true" ? "purecap" : "hybrid";
     const certificate = goodCert === "true" ? "goodCert" : "maliciousCert";
-    const ScriptPath = scriptPaths[mode][certificate];
-    if (ScriptPath) {
+    let ScriptPath = null;
+    let scriptPath = scriptCache[mode][certificate];
+    if (scriptPath) {
       try {
-        const sshOpts = { username, host, port };
-        const scriptPath = new ScriptPath({ sshOpts });
-        await scriptPath.setup({certDirectory: "../openssl-vuln-poc/certs/"});
         const {
           server: { stdin, stdout, stderr },
         } = await scriptPath.run({ port });
@@ -27,9 +36,25 @@ api.get(
         res.status(500).json(error.message);
       }
     } else {
-      res
-        .status(501)
-        .json(`${certificate} certificate for ${mode} mode is not implemented`);
+      ScriptPath = scriptPaths[mode][certificate];
+      if (ScriptPath) {
+        try {
+          const sshOpts = { username, host, port };
+          scriptPath = new ScriptPath({ sshOpts });
+          await scriptPath.setup({certDirectory: "../openssl-vuln-poc/certs/"});
+          scriptCache[mode][certificate] = scriptPath;
+          const {
+            server: { stdin, stdout, stderr },
+          } = await scriptPath.run({ port });
+          res.send({ stdin, stdout, stderr });
+        } catch (error) {
+          res.status(500).json(error.message);
+        }
+      } else {
+        res
+          .status(501)
+          .json(`${certificate} certificate for ${mode} mode is not implemented`);
+      }
     }
   }
 );
