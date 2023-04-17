ff347bb0734ffaa333d6c92b84dedc036bc8c93a
antmcc49 <antmcc49@gmail.com>
2023-04-17 15:42:07 +0100

    chore(app): prettier

--- packages/app/src/modules/cyber-demo/demo-descriptor.tsx
+++ packages/app/src/modules/cyber-demo/demo-descriptor.tsx
@@ -2,22 +2,23 @@ import {Typography} from "@mui/material";
 import {Paper} from "@/modules/common";
 import Image from "next/image";
 
-
 export default function DemoDescriptor() {
   return (
     <Paper>
       <h1>Interactive Demo description</h1>
       <Typography>
         This demo shows how the same exploit issued against a PureCap version of
-        OpenSSL on a Morello CPU, actively defends against exploits the same exploit
-        on an unprotected system results in unprotected memory based exploit.
+        OpenSSL on a Morello CPU, actively defends against exploits the same
+        exploit on an unprotected system results in unprotected memory based
+        exploit.
       </Typography>
       <Image
         src="/ssh-design.svg"
         alt="OpenSSL memory protection diagram"
         width={800}
         height={400}
-        priority/>
+        priority
+      />
     </Paper>
   );
 }
--- packages/app/src/modules/cyber-demo/info-pane.tsx
+++ packages/app/src/modules/cyber-demo/info-pane.tsx
@@ -6,13 +6,14 @@ export default function InfoPane() {
     <Paper sx={{marginBottom: 0}}>
       <h1>OpenSSL Morello protection</h1>
       <Typography>
-        CVE-2022-3602 is a recent OpenSSL exploit, causing significant disruption in the cybersecurity
-        space due to a suspected widespread remote code execution exploit
-        CHERI is a novel computer architecture, supporting a novel ISA
-        (Instruction set architecture) that protects against memory/pointer errors
-        Morello is optimised research silicon that implements the CHERI instruction set.
-        The practical question: can existing software be deployed to a Morello CPU based
-        system which actively defends against common memory exploits
+        CVE-2022-3602 is a recent OpenSSL exploit, causing significant
+        disruption in the cybersecurity space due to a suspected widespread
+        remote code execution exploit CHERI is a novel computer architecture,
+        supporting a novel ISA (Instruction set architecture) that protects
+        against memory/pointer errors Morello is optimised research silicon that
+        implements the CHERI instruction set. The practical question: can
+        existing software be deployed to a Morello CPU based system which
+        actively defends against common memory exploits
       </Typography>
     </Paper>
   );
