3f14585f00eb45a8c7ea59f10003d48d6a484711
Alois Klink <alois@nquiringminds.com>
2023-04-18 16:40:34 +0100

    feat(server): implement test cases with safe certs
    Implement the OpenSSL test cases with the "goodCerts", as these
    were previously mocked.

--- packages/server/src/test-cases.mjs
+++ packages/server/src/test-cases.mjs
@@ -1,26 +1,19 @@
 import {
-  LocalHostTestCase,
   // MorelloPurecapOpenSSLTestCase, // MOCKED SINCE BROKEN
+  MorelloPurecapOpenSSLTestCaseSafeCert,
   MorelloHybridOpenSSLTestCase,
+  MorelloHybridOpenSSLTestCaseSafeCert,
 } from "@nqminds/openssl-vuln-poc";
 
 import { MorelloPurecapOpenSSLTestCase } from "@nqminds/openssl-vuln-poc/src/__mocks__/index.mjs";
 
-/**
- * Mocked up Good certificate test case.
- *
- * We expect that the current server is Ubuntu 22.04, and that openssl has been
- * installed with `apt install openssl`.
- */
-const MockedUpGoodCertTestCase = LocalHostTestCase;
-
 export default {
   purecap: {
-    goodCert: MockedUpGoodCertTestCase,
+    goodCert: MorelloPurecapOpenSSLTestCaseSafeCert,
     maliciousCert: MorelloPurecapOpenSSLTestCase,
   },
   hybrid: {
-    goodCert: MockedUpGoodCertTestCase,
+    goodCert: MorelloHybridOpenSSLTestCaseSafeCert,
     maliciousCert: MorelloHybridOpenSSLTestCase,
   },
 };
