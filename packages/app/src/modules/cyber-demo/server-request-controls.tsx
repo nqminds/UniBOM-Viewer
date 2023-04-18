aee591bb4de76184438c29a380aafffc8836f525
Alois Klink <alois@nquiringminds.com>
2023-04-18 10:46:52 +0100

    refactor(app): simplify ServerRequestControls
    Simplify ServerRequestControls code by using JSX to write the
    description of each server.

--- packages/app/src/modules/cyber-demo/server-request-controls.tsx
+++ packages/app/src/modules/cyber-demo/server-request-controls.tsx
@@ -19,16 +19,22 @@ const ButtonContainer = styled("div")(() => ({
 const servers = [
   {
     name: "Morello Purecap",
-    description:
-      "Sends the given certificates to an OpenSSL v3.0.2 server running on " +
-      "Morello in Purecap (Pure-capability) mode. " +
-      "The malicious certificate will attempt to run a stack buffer " +
-      "overflow. " +
-      "However, the pointer to the buffer has been tagged with the a " +
-      "capability that lists the size of the buffer. " +
-      "Attempting to overflow the buffer will violate the stack " +
-      "pointer capabilities, resulting in the CPU throwing a SIGPROT " +
-      "CHERI protection violation.",
+    description: (
+      <Typography>
+        Sends the given certificates to an OpenSSL v3.0.2 server running on
+        Morello in Purecap (Pure-capability) mode.
+
+        The malicious certificate will attempt to run a stack buffer
+        overflow.
+
+        However, the pointer to the buffer has been tagged with the a
+        capability that lists the size of the buffer.
+
+        Attempting to overflow the buffer will violate the stack
+        pointer capabilities, resulting in the CPU throwing a SIGPROT
+        CHERI protection violation.
+      </Typography>
+    ),
     controls: [
       {
         name: "Safe Cert",
@@ -42,16 +48,21 @@ const servers = [
   },
   {
     name: "Morello Hybrid",
-    description:
-      "Sends the given certificates to an OpenSSL v3.0.2 server running on " +
-      "Morello in Hybrid (hybrid-capability) mode. " +
-      "As Morello runs CheriBSD (a fork of FreeBSD v14), " +
-      "Address Space Layout Randomization is " +
-      "enabled and binaries are compiled with stack protection canaries " +
-      "(such as `-fstack-protector`). " +
-      "Therefore, attempting to exploit CVE-2022-3602 will normally result " +
-      "in the OS catching the attempted buffer overflow, and aborting the " +
-      "process with an 'SIGABRT'.",
+    description: (
+      <Typography>
+        Sends the given certificates to an OpenSSL v3.0.2 server running on
+        Morello in Hybrid (hybrid-capability) mode.
+
+        As Morello runs CheriBSD (a fork of FreeBSD v14),
+        Address Space Layout Randomization is
+        enabled and binaries are compiled with stack protection canaries
+        (such as `-fstack-protector`).
+
+        Therefore, attempting to exploit CVE-2022-3602 will normally result
+        in the OS catching the attempted buffer overflow, and aborting the
+        process with an 'SIGABRT'.
+      </Typography>
+    ),
     controls: [
       {
         name: "Safe Cert",
@@ -71,7 +82,7 @@ export default function ServerRequestControls({mutateRequest, loading}: props) {
       {servers.map(({name, description, controls}) => (
         <Paper key={`server-${name}`}>
           <h2>{name}</h2>
-          <Typography>{description}</Typography>
+          {description}
           <Container>
             {controls.map(({name: buttonName, mutateParams}) => (
               <ButtonContainer key={`${name}-control-${buttonName}`}>
