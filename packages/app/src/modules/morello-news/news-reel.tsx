ee9d856f303640d1f3d54f01c2d836a0575e0057
Alois Klink <alois@nquiringminds.com>
2023-04-24 20:45:59 +0100

    feat(app): use animated news carousel
    Code was adapted from
    https://mui.com/material-ui/react-stepper/#text-with-carousel-effect
    under the MIT license.

--- packages/app/src/modules/morello-news/news-reel.tsx
+++ packages/app/src/modules/morello-news/news-reel.tsx
@@ -1,14 +1,30 @@
-import {useState, useEffect} from "react";
+import {useState} from "react";
 import newsReel from "./articles";
 import {styled} from "@mui/system";
 import Excerpt from "./excerpt";
 import Image from "next/image";
-import {Paper, IconButton} from "@mui/material";
-import {ArrowForwardIos, ArrowBackIos} from "@mui/icons-material";
+import {Paper, IconButton, MobileStepper, useTheme} from "@mui/material";
+import {KeyboardArrowLeft, KeyboardArrowRight} from "@mui/icons-material";
+
+/**
+ * Code adapted from https://mui.com/material-ui/react-stepper/#text-with-carousel-effect
+ *
+ * SPDX-FileCopyrightText: © 2023 Nquiringminds Ltd.
+ * SPDX-FileCopyrightText: Copyright (c) 2014 Call-Em-All
+ * SPDX-License-Identifier: MIT License
+ */
+
+// @ts-expect-error No types, but we can use react-swipeable-views types
+import SwipeableViews from "react-swipeable-views-react-18-fix";
+import {autoPlay} from "react-swipeable-views-utils";
+
+const AutoPlaySwipeableViews = autoPlay(
+  SwipeableViews as typeof import("react-swipeable-views").default,
+);
 
 const News = styled(Paper)(() => ({
   display: "flex",
-  flexDirection: "row",
+  flexDirection: "column",
   height: "100%",
 }));
 
@@ -32,13 +48,9 @@ const Button = styled(IconButton)(({theme: {palette}}) => ({
   color: palette.text.secondary,
 }));
 
-const Next = styled(Button)(() => ({
-  justifySelf: "end",
-}));
-
 export default function NewsReel() {
+  const theme = useTheme();
   const [articleNumber, setArticleNumber] = useState(0);
-  const [currentArticle, setCurrentArticle] = useState(newsReel[0]);
 
   function nextArticle(newNumber: number) {
     if (newNumber >= newsReel.length) {
@@ -50,36 +62,66 @@ export default function NewsReel() {
     }
   }
 
-  useEffect(() => {
-    const interval = setTimeout(() => {
-      nextArticle(articleNumber + 1);
-    }, 10000);
-    return () => clearTimeout(interval);
-  }, [articleNumber]);
-
-  useEffect(() => {
-    setCurrentArticle(newsReel[articleNumber]);
-  }, [articleNumber]);
-
   return (
     <News>
-      <Button onClick={() => nextArticle(articleNumber - 1)}>
-        <ArrowBackIos />
-      </Button>
-      <Container>
-        <ImageContainer>
-          <Image
-            src={currentArticle.imagery}
-            alt={currentArticle.imagery}
-            width={300}
-            height={300}
-          />
-        </ImageContainer>
-        <Excerpt {...currentArticle} />
-      </Container>
-      <Next onClick={() => nextArticle(articleNumber + 1)}>
-        <ArrowForwardIos />
-      </Next>
+      <AutoPlaySwipeableViews
+        axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+        index={articleNumber}
+        onChangeIndex={(x: number) => setArticleNumber(x)}
+        interval={10000} /* Move every 10 seconds */
+        enableMouseEvents
+      >
+        {newsReel.map((article, index) => (
+          <div key={article.name}>
+            {Math.abs(articleNumber - index) <= 2 ? (
+              <Container>
+                <ImageContainer>
+                  <Image
+                    src={article.imagery}
+                    alt={article.imagery}
+                    width={300}
+                    height={300}
+                  />
+                </ImageContainer>
+                <Excerpt {...article} />
+              </Container>
+            ) : null}
+          </div>
+        ))}
+      </AutoPlaySwipeableViews>
+      <MobileStepper
+        steps={newsReel.length}
+        position="static"
+        activeStep={articleNumber}
+        nextButton={
+          <Button
+            size="small"
+            onClick={() => nextArticle(articleNumber + 1)}
+            disabled={articleNumber === newsReel.length - 1}
+          >
+            Next
+            {theme.direction === "rtl" ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </Button>
+        }
+        backButton={
+          <Button
+            size="small"
+            onClick={() => nextArticle(articleNumber - 1)}
+            disabled={articleNumber === 0}
+          >
+            {theme.direction === "rtl" ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+            Back
+          </Button>
+        }
+      />
     </News>
   );
 }
