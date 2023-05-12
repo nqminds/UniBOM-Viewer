25fb65ab7a01d2810a0bc1ec6b16842271aba24f
Alois Klink <alois@nquiringminds.com>
2023-05-12 13:32:41 +0100

    build(lint): disable jsdoc type rules for TS files
    There's no need to put type information into a JSDoc comment if you're
    writing TypeScript.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,18 @@ module.exports = {
     node: true,
   },
   extends: ["@nqminds/eslint-config", "prettier"],
-  overrides: [],
+  overrides: [
+    {
+      // TypeScript files shouldn't have JSDoc types
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "jsdoc/no-types": 1,
+        "jsdoc/require-param-type": 0,
+        "jsdoc/require-property-type": 0,
+        "jsdoc/require-returns-type": 0,
+      },
+    },
+  ],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
