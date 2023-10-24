b4e4b6e8a0db494325cd90c2ed7f69215d0b8d27
ionut-cmd <bostanionut28@gmail.com>
2023-10-24 15:31:06 +0100

    Fix: remove unused code

--- packages/app/src/modules/cve/sbom-component-table-row.tsx
+++ packages/app/src/modules/cve/sbom-component-table-row.tsx
@@ -8,7 +8,6 @@ import {classifySeverityScore, mitigated} from "./severity-map";
 import {ExpandLess, ExpandMore} from "@mui/icons-material";
 import CveTable from "./cve-table";
 import {uniqBy} from "lodash";
-import { Console } from "console";
 
 const Controls = styled(Icon)(({theme: {palette}}) => ({
   background: palette.text.primary,
@@ -45,8 +44,6 @@ export default function SbomComponentTableRow({
   const categorisedCves = data.cves.map(categoriseCves);
   const memorySafeCves = categorisedCves.map((cve) => {
     const {cwes} = cve;
-    // leve this here temoprarly
-    // if (cwes.find(({memoryCwe}) => memoryCwe)) {
       if (Array.isArray(cwes) && cwes.find(({memoryCwe}) => memoryCwe)){
       return {...cve, baseSeverity: mitigated};
     }
