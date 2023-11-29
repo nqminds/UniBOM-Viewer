e689a609fae8a2e9ce38083b9f8c21afe31a7298
anthonymccaigue <anthony@nquiringminds.com>
2023-11-29 11:15:06 +0000

    redirect to https

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://sbom.nqminds.com",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
